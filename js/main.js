80c65bcd4b6662d23f7d99962e96c99ec3ded8ce
Marc Nitzsche <marc.nitzsche@live.de>
2017-12-14 00:44:30 +0100

    Remove unneeded scripts/styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-  outdatedBrowser({
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform',
-        languagePath: ''
-    });
   $('.video').vide('img/whiteboard/White-Board', {
     posterType: 'jpg',
     resizing: true,

cb1287dd0522e81c0d1fedef573744dbaa2a349c
Marc Nitzsche <marc.nitzsche@live.de>
2019-08-18 14:27:28 +0200

    Minify js files

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  "presets": [["@babel/preset-env", {
-        "targets": "> 0.25%, not dead"
-      }]]
-}
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: "> 0.25%, not dead"
+      }
+    ],
+    "minify"
+  ]
+};

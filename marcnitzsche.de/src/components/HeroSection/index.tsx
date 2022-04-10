dc732eeebc088464dc717e05a46b5823bec0868b
Marc N <git@marcnitzsche.de>
2022-04-10 15:52:09 +0200

    Use next/image for background image

--- marcnitzsche.de/src/components/HeroSection/index.tsx
+++ marcnitzsche.de/src/components/HeroSection/index.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
 import clsx from "clsx";
 
 import styles from "./HeroSection.module.scss";
 import textStyles from "styles/text.module.scss";
 import containerStyles from "styles/containers.module.scss";
+
+import backgroundImage from "./beautiful-view.jpg";
 
 const navItems = [
   { title: "Blog", url: "https://blog.marcnitzsche.de" },
@@ -19,6 +22,15 @@ const HeroSection = () => (
     <h1 className={textStyles.H1}>
       <small>Hey,</small> I&apos;m Marc
     </h1>
+
+    <Image
+      src={backgroundImage}
+      layout="fill"
+      sizes="100vw"
+      className={styles.backgroundImage}
+      alt="Person looking at a fjord from the Preikestolen"
+      draggable={false}
+    />
   </section>
 );
 

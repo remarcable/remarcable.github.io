e77b727865e633efa5139e62c1f0b8db54718ed4
Marc N <git@marcnitzsche.de>
2022-04-10 12:06:27 +0200

    Remove unnecessary return statement

--- marcnitzsche.de/src/components/PageHead.tsx
+++ marcnitzsche.de/src/components/PageHead.tsx
@@ -11,47 +11,28 @@ const page = {
   themeColor: "#103981",
 };
 
-const PageHead = () => {
-  return (
-    <Head>
-      <meta charSet="utf-8" />
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta
-        name="google-site-verification"
-        content={GOOGLE_SITE_VERIFICATION}
-      />
-      <title>{page.title}</title>
-      <meta name="description" content={page.description} />
-      <meta name="subject" content="person, cv" />
-      <link rel="author" href="humans.txt" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/manifest.json" />
-      <link
-        rel="mask-icon"
-        href="/safari-pinned-tab.svg"
-        color={page.themeColor}
-      />
-      <meta name="theme-color" content={page.themeColor} />
-      <link rel="canonical" href={ROOT_URL} />
-    </Head>
-  );
-};
+const PageHead = () => (
+  <Head>
+    <meta charSet="utf-8" />
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="google-site-verification" content={GOOGLE_SITE_VERIFICATION} />
+    <title>{page.title}</title>
+    <meta name="description" content={page.description} />
+    <meta name="subject" content="person, cv" />
+    <link rel="author" href="humans.txt" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/manifest.json" />
+    <link
+      rel="mask-icon"
+      href="/safari-pinned-tab.svg"
+      color={page.themeColor}
+    />
+    <meta name="theme-color" content={page.themeColor} />
+    <link rel="canonical" href={ROOT_URL} />
+  </Head>
+);
 
 export default PageHead;

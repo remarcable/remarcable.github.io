ca6c3982d36d81fa0fedba8a2807903b72f0a288
Marc N <git@marcnitzsche.de>
2022-04-10 12:00:26 +0200

    Move styles to next

--- marcnitzsche.de/src/pages/_app.tsx
+++ marcnitzsche.de/src/pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from "next/app";
 import "normalize.css";
 import "styles/global.scss";
 
+// Temporary global imports:
+import "styles/sections/_astronaut.scss";
+import "styles/sections/_footer.scss";
+import "styles/sections/_head.scss";
+import "styles/sections/_me.scss";
+import "styles/sections/_references.scss";
+import "styles/sections/_sections.scss";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }

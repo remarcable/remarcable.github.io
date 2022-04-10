dbe4317d3687754137df6f952ba5a021a040687e
Marc N <git@marcnitzsche.de>
2022-04-10 11:46:22 +0200

    Move favicons and header data to next

---
+++ marcnitzsche.de/src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300&display=optional"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Merriweather:300,300italic,700italic&display=optional"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
